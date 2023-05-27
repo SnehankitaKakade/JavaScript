15c0bf4df490723c5008f34c8773422fa44b3c2c
Snehankita Kakade <kakadesnehankita9810@gmail.com>
2023-05-28 01:00:15 +0530

    JavaScript Second Code

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,36 @@
+
+
+var studentRollNo=1;
+var age=24;
+var percentage=78.1;
+
+var studentName="Abc Sharma";
+var branch="Computer Science";
+var favSubject="java";
+
+var ismarried=true;
+var isWorking=false;
+var isGraduated=true;
+console.log("studentRollNo  :",studentRollNo,",",typeof studentRollNo);
+console.log("age            :",age,",",typeof age);
+console.log("percentage     :",percentage,",",typeof percentage);
+
+console.log("studentName    :",studentName,",",typeof studentName);
+console.log("branch         :", branch,",",typeof  branch);
+console.log("favSubject     :",favSubject,",",typeof favSubject);
+
+console.log("isGraduated    :",isGraduated,",",typeof isGraduated);
+console.log("isWorking      :",isWorking,",",typeof isWorking);
+console.log("ismarried      :",ismarried,",",typeof ismarried);
+
+console.log("");
+
+console.log("*****Difference between null & undefined*****");
+
+console.log("");
+var numVar;
+console.log("numVar -","value:",numVar,",","type :",typeof numVar);
+var numVar=null;
+console.log("numVar -","value:",numVar,",","type:",typeof numVar);
+
+
