fc57fd8961c6a408888adc6b63cd1581118a4c17
Snehankita Kakade <kakadesnehankita9810@gmail.com>
2023-05-28 07:06:17 +0530

    Variable Swapping

---
+++ 02_swapVariables.js
@@ -0,0 +1,26 @@
+
+
+var sweety="Sweety";
+var cutie="Cutie";
+
+console.log("Before Swap..", "sweety: ", sweety, " cutie: ",cutie);
+
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("After Swap..", "sweety: ", sweety, " cutie: ",cutie);
+
+var num1=100;
+var num2=200;
+var num3=300;
+
+console.log("Before Swap..","num1: ",num1," num2: ",num2," num3:",num3);
+ 
+var temp1=num1;
+num1=num2;
+num2=num3;
+num3=temp1;
+
+console.log("After Swap..", "num1: ", num1, " num2: ",num2," num3: ",num3);
+
+
