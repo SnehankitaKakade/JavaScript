45890927dcbf24fa47d16d2f4d4b3a657951a06c
Snehankita Kakade <kakadesnehankita9810@gmail.com>
2023-06-08 16:12:10 +0530

    while loop

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,63 @@
+console.log(`*****************Using while loop*****************`);
+//WAP to print numbers from 5 to 15  by incrementing 1
+console.log(`WAP to print numbers from 5 to 15  by incrementing 1`);
+let i=5;
+while(i<=15){
+    console.log(i);
+    i++;
+}
+
+
+//WAP to print numbers from 50 to 40  by decrementing 1
+console.log(`WAP to print numbers from 50 to 40  by decrementing 1`);
+let j=50;
+while(j>=40){
+    console.log(j);
+    j--;
+}
+
+
+//WAP to find first 15 odd numbers 
+console.log(`WAP to find first 15 odd numbers `);
+ j=1;
+while(j<=15){
+        console.log(j*2-1);
+        j++;
+}
+
+
+//WAP to find first 10 even numbers 
+console.log(`WAP to find first 10 even numbers `);
+j=1;
+while(j<=10){
+        console.log(j*2);
+        j++;
+}
+
+//WAP to print table of 5  like 5 10 15 20 ...50
+console.log(`WAP to print table of 5  like 5 10 15 20 ...50`);
+j=1;
+while(j<=10){
+    let mul=j*5;
+    console.log(mul);
+    j++;
+}
+
+
+
+//WAP to print table of 10  like 10 20 30 ...100
+console.log(`WAP to print table of 10  like 10 20 30 ...100`);
+j=1;
+while(j<=10){
+     mul=j*10;
+    console.log(mul);
+    j++;
+}
+//WAP to print table of 10 in reverse order  like 100 90 80 .....10
+console.log(`WAP to print table of 10 in reverse order  like 100 90 80 .....10`);
+j=10;
+while(j>=1){
+     mul=j*10;
+    console.log(mul);
+    j--;
+}
